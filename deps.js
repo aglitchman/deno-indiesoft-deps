f9e1e403c8c3758029ca2025403380f13233b874
Artsiom Trubchyk <aglitchman@gmail.com>
2021-05-30 15:16:10 +0300

    Update deps.js

--- deps.js
+++ deps.js
@@ -15,6 +15,10 @@ function error(status, err) {
 
 async function handleRequest(request) {
   const { pathname, searchParams } = new URL(request.url);
+  
+  if (request.headers.host.indexOf(".deno.dev") > -1) {
+    return error(403, "Invalid host");
+  }
 
   if (pathname == "/") {
     return new Response(
